7c53d1653a81b1144ecf4be506f2036979fafd59
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-10-06 19:05:39 -0300

    feat: inclui funcoes acessoras

--- src/SimpleJSON.hs
+++ src/SimpleJSON.hs
@@ -12,5 +12,25 @@ getString::JValue -> Maybe String
 getString (JString s) = Just s
 getString _ = Nothing
 
+getInt :: Integral a => JValue -> Maybe a
+getInt (JNumber n) = Just (truncate n)
+getInt _ = Nothing
+
+getDouble :: JValue -> Maybe Double
+getDouble (JNumber n) = Just n
+getDouble _ = Nothing
+
+getBool :: JValue -> Maybe Bool
+getBool (JBool b) = Just b
+getBool _ = Nothing
+
+getObject :: JValue -> Maybe [(String, JValue)]
+getObject (JObject o) = Just o
+getObject _ = Nothing
+
+getArray :: JValue -> Maybe [JValue]
+getArray (JArray a) = Just a
+getArray _ = Nothing
+
 hello :: String
 hello = "Olá do módulo SimpleJSON!"
