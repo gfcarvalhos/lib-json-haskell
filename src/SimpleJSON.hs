eff09b739bd0ad5849ebecaa5f8c52ae463c0fcb
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-10-06 07:03:08 -0300

    feat: inclui dados algebricos para os valores possiveis do json e funcao string

--- src/SimpleJSON.hs
+++ src/SimpleJSON.hs
@@ -1,4 +1,16 @@
 module SimpleJSON where
 
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
+
+getString::JValue -> Maybe String
+getString (JString s) = Just s
+getString _ = Nothing
+
 hello :: String
 hello = "Olá do módulo SimpleJSON!"
